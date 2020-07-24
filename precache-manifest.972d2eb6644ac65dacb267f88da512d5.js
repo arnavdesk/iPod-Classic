676ae6445f64a1b2cedf7bc018ab624059728cd4
arnavdesk <arnavdesk@gmail.com>
2020-07-24 21:23:15 +0530

    Updates

---
+++ precache-manifest.972d2eb6644ac65dacb267f88da512d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0113524ddabd2f82b5aaf23fecd88dd8",
+    "url": "/iPod-Classic/index.html"
+  },
+  {
+    "revision": "2c559740f716b3c437d1",
+    "url": "/iPod-Classic/static/css/main.ae8589cd.chunk.css"
+  },
+  {
+    "revision": "beb845b7a58d6242b3a1",
+    "url": "/iPod-Classic/static/js/2.0c916079.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-Classic/static/js/2.0c916079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c559740f716b3c437d1",
+    "url": "/iPod-Classic/static/js/main.0d823f20.chunk.js"
+  },
+  {
+    "revision": "4deacc06a44225f75abb",
+    "url": "/iPod-Classic/static/js/runtime-main.6904ec8c.js"
+  }
+]);
